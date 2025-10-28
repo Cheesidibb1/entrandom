f0d6b4cedc7721c9a82f87aa71ec9d9e19c978c4
Cheesidibbl <ztech.z@icloud.com>
2025-10-27 17:48:46 -0700

    Add base64 encoding functionality and update README and requirements

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import time
+import pybase64 as base64
 
 cam = cv2.VideoCapture(0)
 if not cam.isOpened():
@@ -21,3 +22,13 @@ def imagecapture():
         cam.release()
         cv2.destroyAllWindows()
 
+def base64encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        base64_bytes = base64.b64encode(image_file.read())
+        base64_string = base64_bytes.decode("utf-8")
+        return base64_string
+
+
+if __name__ == '__main__':
+    imagecapture()
+    print(base64encode_image("output/captured_image.png"))
--- readme.md
+++ readme.md
@@ -4,6 +4,8 @@
 
 Hello there! this is Entrandom, a project aiming toward a *near* perfect CSPRNG
 
-This is based off of Cloudflare's lava lamps, now making it open source
+This is based off of Cloudflare's lava lamps, now making it open source.
 
 This is also a little side project so we will see where we go with it!
+
+I decided to name this project *Entrandom* because of entropy, the lack of order or predictability
